0645a65d6f14d13a329131093aa59713d9984e73
Michael <michaelwilliamtang@gmail.com>
2020-03-16 14:33:02 -0700

    Added plotrix stderror, created cyl/mean_disp graph with error bars, commented

--- TestGraph.R
+++ TestGraph.R
@@ -1,11 +1,20 @@
 library(tidyverse)
+library(plotrix)
 data("mtcars")
 glimpse(mtcars)
 
 mtcarsGroup = mtcars %>% group_by(cyl) %>%
   mutate(mean_disp = mean(disp),
-            max_mpg = max(mpg))
+            max_mpg = max(mpg),
+         std_error = std.error(disp))
 
+# plot max_mpg
 ggplot(mtcarsGroup, aes(x = hp, y = max_mpg, color = gear, group = gear)) +
   geom_line() +
   facet_wrap(~vs)
+
+# plog mean_disp
+ggplot(mtcarsGroup) +
+  geom_line(aes(x = cyl, y = mean_disp, color = carb, group = carb)) +
+  geom_errorbar(aes(x = cyl, ymin = mean_disp - std_error, ymax = mean_disp + std_error, color = carb, group = carb))
+  
