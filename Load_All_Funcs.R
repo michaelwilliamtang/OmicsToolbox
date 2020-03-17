77526612a236b8b20db10ee1a55a874ed9c61075
Michael <michaelwilliamtang@gmail.com>
2020-03-16 21:44:29 -0700

    Quiet scanning

--- Load_All_Funcs.R
+++ Load_All_Funcs.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(stringr)
 
 # ran varvectors
-fibers <- scan(file.path("Metadata", "Fibers.tsv"), character(), quote = '', sep = "\t")
-ids <- scan(file.path("Metadata", "Ids.tsv"), character(), quote = '', sep = "\t")
-timepoints <- scan(file.path("Metadata", "Timepoints.tsv"), character(), quote = '', sep = "\t")
-datasets <- scan(file.path("Metadata", "Datasets.tsv"), character(), quote = '', sep = "\t")
-small_datasets <- scan(file.path("Metadata", "Small_Datasets.tsv"), character(), quote = '', sep = "\t")
+fibers <- scan(file.path("Metadata", "Fibers.tsv"), character(), quote = '', sep = "\t", quiet = T)
+ids <- scan(file.path("Metadata", "Ids.tsv"), character(), quote = '', sep = "\t", quiet = T)
+timepoints <- scan(file.path("Metadata", "Timepoints.tsv"), character(), quote = '', sep = "\t", quiet = T)
+datasets <- scan(file.path("Metadata", "Datasets.tsv"), character(), quote = '', sep = "\t", quiet = T)
+small_datasets <- scan(file.path("Metadata", "Small_Datasets.tsv"), character(), quote = '', sep = "\t", quiet = T)
 
 # loads all core functions (labeled _FUNC), in working dir
 wd_files <- list.files()
@@ -16,9 +16,10 @@ wd_funcs <- wd_files[grep("FUNC[.]R$", wd_files)]
 for (func in wd_funcs) source(func)
 
 # load convenient analyte lists
-all_bile_acids <- scan(file.path("Metadata", "Analyte_Lists", "All_Bile_Acids.tsv"), character(), quote = '', sep = "\t")
-sel_bile_acids <- scan(file.path("Metadata", "Analyte_Lists", "Select_Bile_Acids.tsv"), character(), quote = '', sep = "\t")
-sel_chols <- scan(file.path("Metadata", "Analyte_Lists", "Select_Chols.tsv"), character(), quote = '', sep = "\t")
-sel_bac <- scan(file.path("Metadata", "Analyte_Lists", "Select_Bac.tsv"), character(), quote = '', sep = "\t")
-all_bac <- scan(file.path("Metadata", "Analyte_Lists", "All_Bac.tsv"), character(), quote = '', sep = "\t")
-sel_all_bac <- scan(file.path("Metadata", "Analyte_Lists", "Select_All_Bac.tsv"), character(), quote = '', sep = "\t")
+source_dir <- file.path("Metadata", "Analyte_Lists")
+all_bile_acids <- scan(file.path("Metadata", "Analyte_Lists", "All_Bile_Acids.tsv"), character(), quote = '', sep = "\t", quiet = T)
+sel_bile_acids <- scan(file.path("Metadata", "Analyte_Lists", "Select_Bile_Acids.tsv"), character(), quote = '', sep = "\t", quiet = T)
+sel_chols <- scan(file.path("Metadata", "Analyte_Lists", "Select_Chols.tsv"), character(), quote = '', sep = "\t", quiet = T)
+sel_bac <- scan(file.path("Metadata", "Analyte_Lists", "Select_Bac.tsv"), character(), quote = '', sep = "\t", quiet = T)
+all_bac <- scan(file.path("Metadata", "Analyte_Lists", "All_Bac.tsv"), character(), quote = '', sep = "\t", quiet = T)
+sel_all_bac <- scan(file.path("Metadata", "Analyte_Lists", "Select_All_Bac.tsv"), character(), quote = '', sep = "\t", quiet = T)
